2f42839ebe2c5db3b2319fd83c23f7ec878b57d0
Tony Tran <codemonkey713@gmail.com>
2021-04-21 23:10:22 -0500

    added navigation tabs from bootstrap to be converted to tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import NavBar from './components/Navbar';
+import NavTabs from './components/NavTabs';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
-  return <div className="App">
-    <NavBar />
-    <Home />
-  </div>;
+  return (
+    <Router>
+      <div>
+        {/* <NavBar /> */}
+        <NavTabs />
+        {/* <Home /> */}
+      </div>
+    </Router>
+  );
 };
 
 export default App;
